ea9ca39d4372c66ada92fa24b34a353c3cc548b4
Alvin121005 <2300016035@webmail.uad.ac.id>
2024-11-02 19:12:39 +0700

    Layout Responsif

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",  
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2D76F9',     
+        secondary: '#EEF4FF',   
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], 
+      }
+    },
+  },
+  plugins: [],
+};
